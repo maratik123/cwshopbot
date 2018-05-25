2ad8d6e519586a43e908792afbf8a0cab8a31fc0
Marat Bukharov <maratik@yandex-team.ru>
2018-05-25 19:52:40 +0300

    Inspect message

--- src/main/java/name/maratik/cw/eu/cwshopbot/application/cwapi/DealsListener.java
+++ src/main/java/name/maratik/cw/eu/cwshopbot/application/cwapi/DealsListener.java
@@ -18,6 +18,7 @@ package name.maratik.cw.eu.cwshopbot.application.cwapi;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.Message;
 import org.springframework.stereotype.Component;
 
 /**
@@ -29,7 +30,7 @@ public class DealsListener {
 
     @SuppressWarnings("MethodMayBeStatic")
     @RabbitListener(queues = "marat321_cwshop_bot_deals")
-    public void processDealsAnnounce(String data) {
-        logger.debug("Received next: {}", data);
+    public void processDealsAnnounce(Message<?> data) {
+        logger.debug("Received message(type={}): {}", data.getPayload().getClass(), data.getPayload());
     }
 }
